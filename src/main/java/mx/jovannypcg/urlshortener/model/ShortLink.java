c352f87efc3d94c73ab24c3e1e4008b980b19147
Jovanny Cruz <jovannypcg@yahoo.com>
2017-05-22 23:28:56 -0500

    Add ShortLink model

---
+++ src/main/java/mx/jovannypcg/urlshortener/model/ShortLink.java
@@ -0,0 +1,39 @@
+package mx.jovannypcg.urlshortener.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ShortLink {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String slug;
+    private String destination;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+}
